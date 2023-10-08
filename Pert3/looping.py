26a3a08c30b96e6a6d01daf53524b3a4227afa1e
root <root@My-EliteBook>
2023-10-08 16:31:06 +0700

    Menambahkan file looping.py

---
+++ Pert3/looping.py
@@ -0,0 +1,12 @@
+print('Program Menghitung Blangan Faktorial')
+Nilai_Mahasiswa = int(input('Masukan Nilai Kamu = '))
+if (Nilai_Mahasiswa > 80):
+    print('Nilai kamu A')
+elif (Nilai_Mahasiswa > 70):
+    print('Nilai kamu B')
+elif (Nilai_Mahasiswa > 60):
+    print('Nilai kamu C')
+elif (Nilai_Mahasiswa > 50):
+    print('Nilai kamu D')
+else :
+    print('Nilai kamu E')
