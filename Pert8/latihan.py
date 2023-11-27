dffaa707722ca339236dea6fb5048293245513ac
root <root@My-EliteBook>
2023-11-27 23:19:24 +0700

    Menambahkan file latihan kriptografi menggunakan vigenere cipher

---
+++ Pert8/latihan.py
@@ -0,0 +1,47 @@
+def generate_alphabet():
+    return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def vigenere_cipher(pesan, key, mode):
+    pesan = pesan.upper()
+    key = key.upper()
+    alphabet = generate_alphabet()
+    hasil = ""
+    i = 0
+
+    for char in pesan:
+        if char in alphabet:
+            posisi_pesan = alphabet.index(char)
+            posisi_key = alphabet.index(key[i])
+
+            if mode == 'enkripsi':
+                posisi_hasil = (posisi_pesan + posisi_key) % 26
+            elif mode == 'deskripsi':
+                posisi_hasil = (posisi_pesan - posisi_key) % 26
+
+            hasil += alphabet[posisi_hasil]
+            i = (i + 1) % len(key)
+        else:
+            hasil += char
+
+    return hasil
+
+# Input dari pengguna
+pesan = input("Masukkan pesan: ")
+key = input("Masukkan key: ")
+
+if pesan.islower() or key.islower():
+    print("Tolong masukkan dengan huruf kapital, Pesan maupun Key.")
+else:
+    # Enkripsi
+    pesan_enkripsi = vigenere_cipher(pesan, key, 'enkripsi')
+    print("\n=========================")
+    print("Pesan:", pesan)
+    print([ord(c) - ord('A') for c in pesan])
+    print("Key:", key)
+    print([ord(c) - ord('A') for c in key])
+    print("Enkripsi:", pesan_enkripsi)
+    print([ord(c) - ord('A') for c in pesan_enkripsi])
+
+    # Deskripsi
+    pesan_deskripsi = vigenere_cipher(pesan_enkripsi, key, 'deskripsi')
+    print("Deskripsi:", pesan_deskripsi)
